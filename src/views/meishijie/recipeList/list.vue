606a11bc9961a9b35581d8e92930dc8825f9161a
guoguihong <guoguihong@qq.com>
2021-09-14 11:44:56 +0800

    增菜谱列表加菜谱iid展示

--- src/views/meishijie/recipeList/list.vue
+++ src/views/meishijie/recipeList/list.vue
@@ -28,6 +28,7 @@
         align="center"
       />
       <el-table-column label="菜谱名称" align="center" prop="recipeName" />
+      <el-table-column label="菜谱id" align="center" prop="id" />
       <el-table-column label="操作" align="center" width="130">
         <template slot-scope="scope">
           <el-button
